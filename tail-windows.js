9d22f77533a210f72e055730cdd73574ab74e2c1
Izdatyi <7038127+Izdatyi@users.noreply.github.com>
2018-10-06 10:52:08 +0300

    update

--- tail-windows.js
+++ tail-windows.js
@@ -57,9 +57,6 @@ module.exports = function(RED) {
                 if (interval !== 0) node.error("'" + node.filename + "' has appeared, following new file");
 
                 var options = {
-                    // logger: console,
-                    // useWatchFile: true,
-                    // follow: true,
                     fsWatchOptions: {
                         persistent: true,
                         interval: (parseInt(node.interval) > 0 ? parseInt(node.interval) : 250)
