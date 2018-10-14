1ab0d5f44406e629a0d344ee816818b30eded075
Izdatyi <7038127+Izdatyi@users.noreply.github.com>
2018-10-15 01:57:05 +0300

    update

--- tail-file.js
+++ tail-file.js
@@ -8,33 +8,53 @@ module.exports = function(RED) {
     function TailFileNode(config) {
         RED.nodes.createNode(this, config);
 
-        this.filename = config.filename || "";
-        this.createFile = config.createFile || false;
-        this.encoding = config.encoding || "";
-        this.mode = config.mode || "";
-        this.split = config.split || false;
-        this.separator = config.separator || "";
-        this.fromBeginning = config.fromBeginning || false;
-        this.flushAtEOF = config.flushAtEOF || false;
-        this.rememberLast = config.rememberLast || false;
-        this.bytes = config.bytes || false;
-        this.maxBytes = config.maxBytes || 0;
-        this.skipBlank = config.skipBlank || false;
-        this.useTrim = config.useTrim || false;
-        this.sendError = config.sendError || false;
-        this.interval = config.interval || 0;
-        var node = this;
-        var tail;
-        var message = {};
-
         const debug = config.debug || false;
+        const logger = config.debug ? console : null;
+
+        const configDef = {
+            filename: this.filename = config.filename || "",
+            createFile: this.createFile = config.createFile || false,
+            encoding: this.encoding = config.encoding || "",
+            mode: this.mode = config.mode || "",
+            split: this.split = config.split || false,
+            separator: this.separator = config.separator || "",
+            fromBeginning: this.fromBeginning = config.fromBeginning || false,
+            flushAtEOF: this.flushAtEOF = config.flushAtEOF || false,
+            rememberLast: this.rememberLast = config.rememberLast || false,
+            lineBytes: this.lineBytes = config.lineBytes || 0,
+            limitSize: this.limitSize = config.limitSize || false,
+            maxBytes: this.maxBytes = config.maxBytes || 0,
+            skipBlank: this.skipBlank = config.skipBlank || false,
+            useTrim: this.useTrim = config.useTrim || false,
+            sendError: this.sendError = config.sendError || false,
+            interval: this.interval = config.interval || 0,
+        };
+        var node = this;
+
+        const chokidarDef = {
+            persistent: true,
+            ignoreInitial: true,
+            usePolling: true,
+            interval: 100,
+            binaryInterval: 300,
+            alwaysStat: true,
+            awaitWriteFinish: {
+                stabilityThreshold: (parseInt(node.interval) > 0 ? parseInt(node.interval) : (node.mode ? 200 : 100)),
+                pollInterval: 100
+            },
+            ignorePermissionErrors: true,
+            atomic: true
+        };
+
+        var message = {};
+        var tail;
+
+        
         if (debug) node.warn(`Start`);
-
-
         node.status({ fill: "grey", shape: "ring", text: "waiting for file" });
         start();
 
-        
+
         function start(callback)
         {
             try {
@@ -47,25 +67,25 @@ module.exports = function(RED) {
                 node.status({ fill: "red", shape: "dot", text: "create file error" });
             }
 
-
-            var options = {
-                logger: config.debug ? console : null,
-                platform: platform,
-                options: {
-                    persistent: true,
-                    interval: (parseInt(node.interval) > 0 ? parseInt(node.interval) : 200)
-                },
-                encoding: (node.encoding.trim() !== "" ? node.encoding.trim() : "utf-8"),
-                separator: (node.split ? RegExp(((node.separator.trim() !== "") ? node.separator.trim() : "[\r]{0,1}\n"), "gi") : ""),
-                fromBeginning: node.fromBeginning,
-                maxBytes: (node.bytes ? ((parseInt(node.maxBytes) > 0) ? parseInt(node.maxBytes) : 5120) : 0),
-                mode: node.mode,
-                flushAtEOF: node.flushAtEOF,
-                rememberLast: (node.mode ? node.rememberLast : false)
-            };
-            if (debug) node.warn(options);
-
             try {
+                var options = {
+                    logger: logger,
+                    platform: platform,
+                    encoding: (node.encoding.trim() !== "" ? node.encoding.trim() : "utf-8"),
+                    separator: (node.split ? RegExp(((node.separator.trim() !== "") ? node.separator.trim() : "[\r]{0,1}\n"), "gi") : ""),
+                    fromBeginning: node.fromBeginning,
+                    maxBytes: (node.limitSize ? ((parseInt(node.maxBytes) > 0) ? parseInt(node.maxBytes) : 5120) : 0),
+                    mode: node.mode,
+                    flushAtEOF: node.flushAtEOF,
+                    rememberLast: (node.mode ? node.rememberLast : false),
+                    lineBytes: ((parseInt(node.lineBytes) > 0) ? parseInt(node.lineBytes) : 512),
+                    chokidar: (node.chokidar ? node.chokidar : chokidarDef)
+                };
+                if (!node.chokidar) {
+                    options.chokidar.awaitWriteFinish.stabilityThreshold = (parseInt(node.interval) > 0 ? parseInt(node.interval) : (node.mode ? 200 : 100))
+                }
+                if (debug) node.warn(options);
+
                 tail = new Tail(node.filename, options);
                 if (tail) {
                     tail.on("line", function (data) {
@@ -145,6 +165,17 @@ module.exports = function(RED) {
         }
 
 
+        this.on('err', function(err, msg = message) {
+            msg.filename = node.filename;
+            node.error(err, msg);
+            if (node.sendError) {
+                var sendMessage = RED.util.cloneMessage(msg);
+                delete sendMessage.payload;
+                sendMessage.error = err;
+                node.send(sendMessage);
+            }
+        })
+
         this.on('close', function(done) {
             stop(function () {
                 node.status({});
@@ -168,24 +199,97 @@ module.exports = function(RED) {
                     break;
                 
                 case "tail-file-filename".toLowerCase():
-                    stop(function () {
-                        node.filename = msg.payload.toString() || "";
-                        start();
-                    });
+                    if ("payload" in msg) {
+                        stop(function () {
+                            node.filename = msg.payload.toString() || "";
+                            start();
+                        });
+                    }
+                    break;
+                
+                case "tail-file-config".toLowerCase():
+                    if (("payload" in msg) && 
+                        (Object.prototype.toString.call(msg.payload).toLowerCase() == "[object Object]".toLowerCase()))
+                    {
+                        node.filename = (("filename" in msg.payload) ? msg.payload.filename.toString() : configDef.filename);
+
+                        node.createFile = ((("createFile" in msg.payload) && (Object.prototype.toString.call(msg.payload.createFile).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.createFile : configDef.createFile);
+
+                        if ("encoding" in msg.payload) {
+                            var encoding = msg.payload.encoding.toLowerCase().trim();
+                            if (
+                                (encoding == "utf8") || (encoding == "utf-8") ||
+                                (encoding == "ucs2") || (encoding == "ucs-2") ||
+                                (encoding == "utf16le") || (encoding == "utf-16le") ||
+                                (encoding == "latin1") || (encoding == "binary") ||
+                                (encoding == "base64") || (encoding == "ascii") ||
+                                (encoding == "hex")
+                            ){
+                                encoding = encoding.replace("utf8", "utf-8");
+                                encoding = encoding.replace("ucs-2", "ucs2");
+                                encoding = encoding.replace("utf-16le", "utf16le");
+                                node.encoding = encoding;
+                            }
+                            else node.encoding = configDef.encoding;
+                        }
+                        else node.encoding = configDef.encoding;
+
+                        node.mode = ((("mode" in msg.payload) && ((msg.payload.mode.toString() === "") || (msg.payload.mode.toLowerCase() == "replaced"))) ? msg.payload.mode.toLowerCase() : configDef.mode);
+
+                        node.split = ((("split" in msg.payload) && (Object.prototype.toString.call(msg.payload.split).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.split : configDef.split);
+
+                        node.separator = (("separator" in msg.payload) ? msg.payload.separator.toString() : configDef.separator);
+
+                        if (("separator" in msg.payload) && !(("split" in msg.payload) && (Object.prototype.toString.call(msg.payload.split).toLowerCase() == "[object Boolean]".toLowerCase())))
+                        {
+                            node.split = true;
+                        }
+
+                        node.fromBeginning = ((("fromBeginning" in msg.payload) && (Object.prototype.toString.call(msg.payload.fromBeginning).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.fromBeginning : configDef.fromBeginning);
+
+                        node.flushAtEOF = ((("flushAtEOF" in msg.payload) && (Object.prototype.toString.call(msg.payload.flushAtEOF).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.flushAtEOF : configDef.flushAtEOF);
+
+                        node.rememberLast = ((("rememberLast" in msg.payload) && (Object.prototype.toString.call(msg.payload.rememberLast).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.rememberLast : configDef.rememberLast);
+
+                        node.lineBytes = (("lineBytes" in msg.payload) ? msg.payload.lineBytes.toString().trim() : configDef.lineBytes);
+
+                        node.limitSize = ((("limitSize" in msg.payload) && (Object.prototype.toString.call(msg.payload.limitSize).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.limitSize : configDef.limitSize);
+
+                        node.maxBytes = (("maxBytes" in msg.payload) ? msg.payload.maxBytes.toString().trim() : configDef.maxBytes);
+
+                        if (("maxBytes" in msg.payload) && !(("limitSize" in msg.payload) && (Object.prototype.toString.call(msg.payload.limitSize).toLowerCase() == "[object Boolean]".toLowerCase())))
+                        {
+                            node.limitSize = true;
+                        }
+
+                        node.skipBlank = ((("skipBlank" in msg.payload) && (Object.prototype.toString.call(msg.payload.skipBlank).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.skipBlank : configDef.skipBlank);
+
+                        node.useTrim = ((("useTrim" in msg.payload) && (Object.prototype.toString.call(msg.payload.useTrim).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.useTrim : configDef.useTrim);
+
+                        if ((("useTrim" in msg.payload) && (Object.prototype.toString.call(msg.payload.useTrim).toLowerCase() == "[object Boolean]".toLowerCase())) && !(("skipBlank" in msg.payload) && (Object.prototype.toString.call(msg.payload.skipBlank).toLowerCase() == "[object Boolean]".toLowerCase())))
+                        {
+                            node.skipBlank = true;
+                        }
+
+                        node.sendError = ((("sendError" in msg.payload) && (Object.prototype.toString.call(msg.payload.sendError).toLowerCase() == "[object Boolean]".toLowerCase())) ? msg.payload.sendError : configDef.sendError);
+
+                        node.interval = (("interval" in msg.payload) ? msg.payload.interval.toString().trim() : configDef.interval);
+
+                        
+                        if (("chokidar" in msg.payload) && (Object.prototype.toString.call(msg.payload.chokidar).toLowerCase() === "[object Object]".toLowerCase())) {
+                            node.chokidar = msg.payload.chokidar;
+                        }
+                        else delete node.chokidar;
+
+
+                        stop(function () {
+                            start();
+                        });
+                    }
+                    else node.emit("err", `incorrect configuration`);
                     break;
             }
         });
-
-        this.on('err', function(err, msg = message) {
-            msg.filename = node.filename;
-            node.error(err, msg);
-            if (node.sendError) {
-                var sendMessage = RED.util.cloneMessage(msg);
-                delete sendMessage.payload;
-                sendMessage.error = err;
-                node.send(sendMessage);
-            }
-        })
     }
 
     RED.nodes.registerType("tail-file", TailFileNode);
